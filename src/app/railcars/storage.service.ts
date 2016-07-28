1f499d9e9840f8dda893c9afea5f0a576ab7b0e1
vadimstetsyuk <boss.boys@list.ru>
2016-07-28 17:07:01 +0300

    add view page on railcars

--- src/app/railcars/storage.service.ts
+++ src/app/railcars/storage.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LocalStorageService {
     private colunmVisibility: string;
+    private colunmVisibilityPrint: string;
 
     constructor() { 
         this.colunmVisibility = 'ColunmVisibility';
-
+        this.colunmVisibilityPrint = 'ColunmVisibilityPrint';
     }
-
 
     public saveViewToStorage(_cols : any ): void {
         window.localStorage.setItem(this.colunmVisibility , JSON.stringify(_cols));
@@ -19,5 +19,13 @@ export class LocalStorageService {
 
     public getViewFromStorage() : any {
         return JSON.parse(window.localStorage.getItem(this.colunmVisibility));
+    } 
+
+    public saveViewPrintToStorage(_cols : any ): void {
+        window.localStorage.setItem(this.colunmVisibilityPrint , JSON.stringify(_cols));
+    }
+
+    public getViewPrintFromStorage() : any {
+        return JSON.parse(window.localStorage.getItem(this.colunmVisibilityPrint));
     } 
 }
