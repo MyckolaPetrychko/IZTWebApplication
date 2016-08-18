6d9c98656b16e69db24eb340897aae20324271fb
Марина Дуда <maryna.duda@gmail.com>
2016-08-18 12:35:18 +0300

    enable prod Mode

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,6 +4,7 @@ import { AppMainComponent }  from './main.component';
 // import { FormsModule }         from '@angular/forms';
 // import {  Http } from '@angular/http';
 // import { TranslateModule, TranslateStaticLoader, TranslateLoader } from 'ng2-translate/ng2-translate';
+import { enableProdMode } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module'
 import { RailcarsModule } from '../railcars/railcar.module';
@@ -11,6 +12,9 @@ import { SettingsModule } from '../settings/settings.module';
 
 import { routerProviders } from './main.routers';
 import {routing} from './main.routers';
+
+enableProdMode();
+
 @NgModule({
   imports: [
     // BrowserModule,
