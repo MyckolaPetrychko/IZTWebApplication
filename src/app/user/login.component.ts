f8d985ef78a263800033930bed261a22ccb229b5
Марина Дуда <maryna.duda@gmail.com>
2016-08-19 15:20:40 +0300

    check login page

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { AuthService } from '../shared/auth/auth.service';
 import { UserService } from '../shared/auth/user.service';
 import { IAuthUser } from '../shared/auth/auth-user.model';
-import { Router /*, ActivatedRoute */} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     moduleId: module.id,
@@ -10,7 +11,7 @@ import { Router /*, ActivatedRoute */} from '@angular/router';
     templateUrl: 'login.component.html'
     // styles : ['login.component.css']
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit, OnDestroy {
     private user: { username: string, password: string };
 
     private message: string;
@@ -18,48 +19,78 @@ export class UserLoginComponent {
     private time: number;
     private visibility: boolean;
 
+    private mainModal: boolean;
+    private isErrRouterMess: boolean;
+    private errRouterMess: boolean;
+
+    private _roterSub: Subscription;
 
     constructor(
         private _user: UserService,
         private _auth: AuthService,
-        private _route: Router) {
+        private _route: Router,
+        private _router: ActivatedRoute) {
 
+        this.isErrRouterMess = false;
         this.visibility = false;
-        this.setMessage('MESSAGE.LOADING', 5, 'info');
+        this.mainModal = true;
+        // this.setMessage('MESSAGE.LOADING', 0, 'info');
         this.user = {
             username: null,
             password: null
         }
     }
 
+    ngOnInit() {
+                this.mainModal = true;
 
+        this._roterSub = this._router.params.subscribe(
+            (params: Params): void => {
+                this.errRouterMess = params['message'];
+                this.isErrRouterMess = (!!this.errRouterMess);
+            });
+    }
+
+    ngOnDestroy() {
+        this._roterSub.unsubscribe();
+    }
+
+
+        @HostListener('click') onClick() {
+            // NOTE: fix close event by outside modal click;
+                this.mainModal = true;
+
+    }
 
     public login() {
         this._auth.login(this.user.username, this.user.username)
             .subscribe(
             (data: IAuthUser) => {
                 this._user.changeUser(data);
-               this.setMessage('MESSAGE.SUCESS_AUTH', 5, 'info');
-
-                this._route.navigate(['/railcars']);
+                if (this.isErrRouterMess) {
+                    this.cancel();
+                } else {
+                    this._route.navigate(['/railcars']);
+                }
 
             }, (err: string): void => {
                 this._user.changeUser(null);
-               this.setMessage(err, 0, 'error');
-
+                this.setMessage(err, 0, 'error');
                 console.error(err);
             }
-            );
+        );
     }
 
     private setMessage(_mess: string, _time: number, _type: string) {
-    this.message = _mess;
-    this.type = _type;
-   this.time = _time;
-   this.visibility = true;
+        this.message = _mess;
+        this.type = _type;
+        this.time = _time;
+        this.visibility = true;
     }
 
-    // public logout() {
-    //     this._auth.logout();
-    // }
+    private cancel(): void {
+        window.history.back();
+    }
+
+
 }
